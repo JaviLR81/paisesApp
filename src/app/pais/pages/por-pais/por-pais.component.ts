84709bd4497e112db56bc2cae2e7989621161861
JaviLR81 <javierlzrd@hotmail.com>
2022-01-06 17:45:30 -0600

    Se  captura input en component por-pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  termino: string = 'hello';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(): void {
+    console.log(this.termino);
   }
 
 }
